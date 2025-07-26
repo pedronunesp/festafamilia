6755eea6bcb039813704dd4c77292ced33385ca3
pedronunesp <pedroandrade60874@gmail.com>
2025-07-26 15:39:49 -0300

    teste2

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,7 +1,19 @@
 import { PrismaClient } from '@prisma/client'
 
 const prismaClientSingleton = () => {
-  return new PrismaClient()
+  const dbUrl = process.env.NETLIFY_DATABASE_URL || process.env.NETLIFY_DATABASE_URL_UNPOOLED;
+
+  if (!dbUrl) {
+    console.warn('DATABASE_URL, NETLIFY_DATABASE_URL, or NETLIFY_DATABASE_URL_UNPOOLED environment variable is not set. PrismaClient might not connect correctly.');
+  }
+
+  return new PrismaClient({
+    datasources: {
+      db: {
+        url: dbUrl,
+      },
+    },
+  });
 }
 
 declare global {
